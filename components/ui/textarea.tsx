c389971e3fa9874d45bb487a09fd4b4bc7628b0a
ziyadwma <ziyad@dieture.com>
2025-09-11 16:36:06 +0300

    Update textarea.tsx

--- components/ui/textarea.tsx
+++ components/ui/textarea.tsx
@@ -1,7 +1,10 @@
 import type { TextareaHTMLAttributes } from "react";
 import { cn } from "./utils";
 
-export function Textarea({ className, ...props }: TextareaHTMLAttributes<HTMLTextAreaElement>) {
+export function Textarea({
+  className,
+  ...props
+}: TextareaHTMLAttributes<HTMLTextAreaElement>) {
   return (
     <textarea
       className={cn(
