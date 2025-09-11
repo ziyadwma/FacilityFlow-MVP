b8e1bc566fadbe7fc75a0e2547dba0a58e0a384c
ziyadwma <ziyad@dieture.com>
2025-09-11 17:02:11 +0300

    Update badge.tsx

--- components/ui/badge.tsx
+++ components/ui/badge.tsx
@@ -1,6 +1,17 @@
 import type { HTMLAttributes, PropsWithChildren } from "react";
 import { cn } from "./utils";
 
-export function Badge({ className, ...props }: PropsWithChildren<HTMLAttributes<HTMLSpanElement>>) {
-  return <span className={cn("inline-block text-xs px-2 py-0.5 rounded-md", className)} {...props} />;
+export function Badge({
+  className,
+  ...props
+}: PropsWithChildren<HTMLAttributes<HTMLSpanElement>>) {
+  return (
+    <span
+      className={cn(
+        "inline-block text-xs px-2 py-0.5 rounded-md",
+        className
+      )}
+      {...props}
+    />
+  );
 }
